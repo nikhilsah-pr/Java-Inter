af941e9d507613768f45c2f8f377c98036991644
nikhilsah-pr <nikhilsah236@gmail.com>
2026-01-15 22:41:15 +0530

    logic update

--- arr0.java
+++ arr0.java
@@ -4,11 +4,49 @@ public class arr0
     public static void main(String args [])
     {
         Scanner in =new Scanner (System.in);
-        int n;
+        int n,i;
+        int x=0,y=0;
         System.out.println("Enter Size of array");
         n=in.nextInt();
         int arr[]=new int[n];
+        int num[]=new int[n];
+        int zer[]=new int[n];
         
+       
+        System.out.println("Enter elements in array");
+        for(i=0;i<n;i++)
+        {
+            arr[i]=in.nextInt();
+        }
+        System.out.println("Your original array");
+        for(i=0;i<n;i++)
+        {
+            System.out.println(arr[i]+" , ");
+        }
+        System.out.println("Your original array with - (Move All Zeros to the End)");
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]!=0)
+            {
+                 num[i]=arr[i];
+                 x++;
+            }
+   
+            else
+            {
+                 zer[i]=arr[i];
+                 y++;
+            }
+               
+        }
+        for(i=0;i<x;i++)
+        {
+            System.out.print(num[i]+" , ");
+        }
+        for(i=0;i<(n-x);i++)
+        {
+            System.out.println(zer[i]+" , ");
+        }
 
     }
 }
