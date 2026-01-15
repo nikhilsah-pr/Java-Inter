d996853bbd763786218bb721a6333bcbf160c3af
nikhilsah-pr <nikhilsah236@gmail.com>
2026-01-15 22:13:38 +0530

    array assigned

---
+++ arr0.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class arr0
+{
+    public static void main(String args [])
+    {
+        Scanner in =new Scanner (System.in);
+        int n;
+        System.out.println("Enter Size of array");
+        n=in.nextInt();
+        int arr[]=new int[n];
+        
+
+    }
+}
