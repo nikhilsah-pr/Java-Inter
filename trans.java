d3eb2db0fdc31ab7aa808adf61811375067a6ff8
nikhilsah-pr <nikhilsah236@gmail.com>
2026-01-18 22:19:39 +0530

    newbug

--- trans.java
+++ trans.java
@@ -4,17 +4,27 @@ public class trans
     public static void main(String args[])
     {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter size of");
-        int arr[][]={{5,6,8,8},{4,6,2,7},{6,3,9,1}};
-        System.out.println("Diagonal Elements");
-        for(int i=0;i<4;i++)
+        int i,j;
+        int arr[][]={{5,6,8,8,4},{4,6,2,7,1},{6,3,9,1,2}};
+        System.out.println("OG Array");
+        for(i=0;i<5;i++)
         {
-            for(int j=0;j<4;j++)
+            for(j=0;j<5;j++)
+            {
+                System.out.print(arr[i][j]+" , ");
+            }
+            System.out.println();
+        }
+        System.out.println("Diagonal Elements");
+        for(i=0;i<5;i++)
+        {
+            for(j=0;j<5;j++)
             {
                 if(i==j)
-                {
-                    System.out.print(arr[i][j]+" , ");
-                }
+                    System.out.print(arr[i][j]);
+                else
+                    System.out.print(" ");
+
             }
             System.out.println();
         }
