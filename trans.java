78fd392e43627a2d8b8a7452a11371908c004668
nikhilsah-pr <nikhilsah236@gmail.com>
2026-01-19 22:02:18 +0530

    Done

--- trans.java
+++ trans.java
@@ -28,5 +28,18 @@ public class trans
             }
             System.out.println();
         }
+        System.out.println("Non Diagonal Elements");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                if(!(i==j))
+                    System.out.print(arr[i][j]);
+                else
+                    System.out.print(" ");
+
+            }
+            System.out.println();
+        }
     }
 }
