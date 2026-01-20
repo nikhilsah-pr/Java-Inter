b9cd88b1d8c2ddbff2c497a768c51d684d917164
nikhilsah-pr <nikhilsah236@gmail.com>
2026-01-20 23:45:34 +0530

    add 2

---
+++ reverse.java
@@ -0,0 +1,24 @@
+import  java.util.*;
+public class reverse
+{
+    public static void main(String args[])
+    {
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter a number");
+        int n;
+        n=in.nextInt();
+        if(n==0 && n<0)
+        {
+            System.exit(0);
+        }
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+
+            }
+
+        }
+
+    }
+}
